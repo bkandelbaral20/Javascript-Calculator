f7d60a31c435b5bfbd56a0417c0fffa643d9bec7
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-09-17 21:31:47 -0500

    adding functionality

--- functionality.js
+++ functionality.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     //click event for showing the value in the textarea
     $(".btn").click(function(){
-        ;
+        var attr = $(this).attr("value");
+        $(".text").append(attr);
     });
+
+    $("h1").dblclick(function () {
+        alert("hello");
+
+    })
 });
