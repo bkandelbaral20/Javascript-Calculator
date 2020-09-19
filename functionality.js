d22e92b383503cc8555fd5eacf4b1efe00e191aa
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-09-19 16:04:18 -0500

    adding jquery

--- functionality.js
+++ functionality.js
@@ -1,12 +1,50 @@
 $(document).ready(function(){
-    //click event for showing the value in the textarea
-    $(".btn").click(function(){
-        var attr = $(this).attr("value");
-        $(".text").append(attr);
+    //click event for showing the value (numbers) in the textarea
+    $(".value1").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
+    });
+    $(".value2").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
+    });
+    $(".value3").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
+    });
+    $(".value4").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
+    });
+    $(".value5").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
+    });
+    $(".value6").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
+    });
+    $(".value7").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
+    });
+    $(".value8").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
+    });
+    $(".value9").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
+    });
+    $(".value0").click(function(){
+        var attr = $(this).val();
+        $(".text").val(attr);
     });
 
-    $("h1").dblclick(function () {
-        alert("hello");
+//clear all the input text in the input feild
+    $(".C").click(function(){
+        $('.text').val("");
+    });
 
-    })
+
 });
